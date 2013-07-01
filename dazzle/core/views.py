a8841a04964dfc3451e40e3ba6f8669c27fad3af
Yang Han <d3vanti@hotmai.com>
2013-06-30 20:57:36 -0400

    add requirements script

--- dazzle/core/views.py
+++ dazzle/core/views.py
@@ -1,5 +1,4 @@
 # Create your views here.
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo pip install -r requirements.txt
 python ./dazzle/manage.py runserver [::]:8000
