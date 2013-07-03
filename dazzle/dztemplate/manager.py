f98ac204c24013551662d5e211e28a323277fbbc
Yang Han <d3vanti@hotmai.com>
2013-07-03 18:57:53 -0400

    get template from s3

--- dazzle/dztemplate/manager.py
+++ dazzle/dztemplate/manager.py
@@ -1,6 +1,9 @@
 
 from django.template.loader import render_to_string
+import urllib2 
 
 def get_template_as_string():
-	html_string = render_to_string('dztemplate/home.html')
+
+	response = urllib2.urlopen('http://s3.amazonaws.com/dazzledev/templates/thinksimple/index.html')
+	html_string = response.read() 
 	return html_string
