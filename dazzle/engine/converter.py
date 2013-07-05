6759eb8dd079ca9fa772aca656ad889c9a978c6d
Yang Han <d3vanti@hotmai.com>
2013-07-05 19:29:35 -0400

    disable editing for unwrapped text nodes

--- dazzle/engine/converter.py
+++ dazzle/engine/converter.py
@@ -1,6 +1,7 @@
 from pyquery import PyQuery
 import urlparse   
 import utils.constants as Constants
+from lxml import etree
 from dztemplate.manager import get_template_as_string
 
 # MAIN ENGINE
@@ -25,13 +26,24 @@ class Converter:
 
 	# INCLUDE SCRIPTS FUNCTIONS
 	def include_scripts(self): 
+
+		def has_js(val):
+			e = PyQuery(this)
+			source = e.attr('src')
+
+			if source is None: 
+				return False
+
+			if source.lower().find(val) != -1:
+				return True
+
 		html_obj = self.html_obj
 
-		elements = html_obj('script').filter(lambda: has_js('jquery'))  
-		if len(elements) == 0: 
-			self.add_script(html_obj, Constants.JQUERY_URL)
+		#elements = html_obj('script').filter(lambda: has_js('jquery'))  
+		#if len(elements) == 0: 
+		#	self.add_script(html_obj, Constants.JQUERY_URL)
 		#TEMPORARY: bootstrap only supports jquery 1.7+
-		#self.add_script(Constants.JQUERY_URL)
+		self.add_script(Constants.JQUERY_URL)
 
 		elements = html_obj('script').filter(lambda: has_js('bootstrap'))  
 		if len(elements) == 0: 
@@ -42,6 +54,7 @@ class Converter:
 		self.add_script(Constants.XEDITABLE_JS_URL)
 		self.add_script(Constants.ENGINE_JS_URL)
 
+
 	def add_css(self, source):
 		html_obj = self.html_obj
 		src_str = '<link type="text/css" href="' + source + '" rel="stylesheet"></link>'
@@ -50,21 +63,37 @@ class Converter:
 	def add_script(self, source):
 		html_obj = self.html_obj 
 		src_str = '<script type="text/javascript" src="' + source + '">//</script>'
-		html_obj('head').append(src_str) 
+		html_obj('body').append(src_str) 
  
 	# TEXT FUNCTIONS
 	def set_text_editable(self):
-		html_obj = self.html_obj
+
+		def has_text(i, this):
+			if PyQuery(this).is_('script'): 
+				return False
+
+			text = PyQuery(this).clone().children().remove().end().text()
+			
+			if text is None:
+				return False
+
+			if (len(text.strip(' \t\n\r')) > 0):
+				return True
+
+			return False
  
+		html_obj = self.html_obj
+   
 		elements = html_obj('*').filter(has_text)  
 
 		unique_id = 'dztxt'
+		
 		num = 1
 
-	 	for e in elements:
-	 		e.set('dztype', 'text')
-	 		e.set('dzid', unique_id + str(num))
-	 		num += 1
+		for e in elements: 
+	 		if len(list(e)) == 0:
+	 			e.set('dztype', 'text')
+				e.set('dzid', unique_id + str(num)) 
 
 	# REPLACE LINK FUNCTIONS
 	def replace_local_links(self):
@@ -73,6 +102,10 @@ class Converter:
 
 
 	def replace_link(self, attr):
+
+		def is_absolute(url):
+			return bool(urlparse.urlparse(url.strip()).scheme)
+
 		html_obj = self.html_obj
 
 		location = Constants.S3_TEMPLATE_URL + self.template_name + '/'
@@ -84,37 +117,6 @@ class Converter:
 			if (not is_absolute(value)):  
 				value = location + value
 				e.set(attr, value)
-
-
-# HELPER FUNCTIONS
-
-def has_js(val):
-		e = PyQuery(this)
-		source = e.attr('src')
-
-		if source is None: 
-			return False
-
-		if source.lower().find(val) != -1:
-			return True
-
-def has_text(i, this):
-
-		if PyQuery(this).is_('script'): 
-			return False
-
-		text = PyQuery(this).clone().children().remove().end().text()
-		
-		if text is None:
-			return False
-
-		if (len(text.strip(' \t\n\r')) > 0):
-			return True
-
-		return False
-		
-def is_absolute(url):
-	return bool(urlparse.urlparse(url.strip()).scheme)
 	  
 
 
