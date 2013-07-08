63f861fe5c8c66dfe4135fe14ea8ddbe71feb037
Yang Han <d3vanti@hotmai.com>
2013-07-08 17:33:04 -0400

    start img code

--- dazzle/static/engine/js/engine.js
+++ dazzle/static/engine/js/engine.js
@@ -2,6 +2,42 @@ window.dazzlejQuery = jQuery.noConflict(true);
 
 window.dazzlejQuery(function(){ 
 
+	addTextEditable();
+	addImageEditable();
+});
+
+
+function addImageEditable()
+{
+	var tags = document.getElementsByTagName('*');
+    var element;
+
+	for (var i = 0, len = tags.length; i < len; i++) {
+	    element = tags[i];
+
+	    if (element.nodeName == "IMG")
+	    {
+	    	element.className = 'bg_found';
+	    }
+	    else if (element.currentStyle)
+	    {
+	        if( element.currentStyle['backgroundImage'] !== 'none' ) 
+	        {
+	            element.className += ' bg_found';
+	        }
+	    }
+	    else if (window.getComputedStyle)
+	    {
+	        if( document.defaultView.getComputedStyle(element, null).getPropertyValue('background-image') !== 'none' ) 
+	        {
+	            element.className += ' bg_found';
+	        }
+	    }
+	}
+}
+
+function addTextEditable()
+{
 	var dz$ = window.dazzlejQuery;
 
 	dz$('[dztype="text"]').each(function(index){
@@ -31,13 +67,14 @@ window.dazzlejQuery(function(){
 		});
 
 	});
+}
 
-	var saveData = function(data)
-	{ 
-		var url = 'update' + window.location.pathname;
-		dz$.post(url, data, function(response)
-		{
-			//alert(response);
-		});
-	}
-});
+var saveData = function(data)
+{ 
+	var dz$ = window.dazzlejQuery;
+	var url = 'update' + window.location.pathname;
+	dz$.post(url, data, function(response)
+	{
+		console.log(response); 
+	});
+}
