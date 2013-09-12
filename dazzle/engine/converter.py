f02d12e4fba91c0a7d67f229e03a9441b93a2a06
Yang Han <d3vanti@hotmai.com>
2013-09-12 14:32:49 -0400

    fix merge

--- dazzle/engine/converter.py
+++ dazzle/engine/converter.py
@@ -4,10 +4,7 @@ import utils.constants as Constants
 from lxml import etree
 from dztemplate.manager import get_template_as_string
 from dztemplate.manager import save_template
-<<<<<<< HEAD
-=======
 import urllib
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 import os
 
 # MAIN ENGINE
@@ -18,10 +15,7 @@ class Converter:
 		self.html_obj = PyQuery(template_string.decode('utf-8'))
 		self.file_name = file_name
 		self.template_name = template_name
-<<<<<<< HEAD
-=======
 		self.location = Constants.S3_TEMPLATE_URL + template_name + '/'
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 
 	def run_upload_engine(self):
 		self.insert_element_tags()
@@ -40,47 +34,24 @@ class Converter:
  
 
 	# INCLUDE SCRIPTS FUNCTIONS
-<<<<<<< HEAD
-	def include_scripts(self): 
-		html_obj = self.html_obj
-
-		self.add_scripts([Constants.JQUERY_URL, Constants.DROPZONE_JS_URL] + Constants.HALLO_JS_URLS + [Constants.ENGINE_JS_URL]) 
-		self.add_css([Constants.FONTAWESOME_CSS_URL, Constants.ENGINE_CSS_URL])
-	
-
-	def add_css(self, sources):
-		html_obj = self.html_obj
-=======
 	def include_scripts(self):  
 		self.add_scripts(Constants.ENGINE_JS_INCLUDES) 
 		self.add_css([Constants.FONTAWESOME_CSS_URL, Constants.ENGINE_CSS_URL])
 	
 
 	def add_css(self, sources): 
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 		src_str = "";
 		for src in sources:
 			src_str += '<link type="text/css" href="' + src + '" rel="stylesheet"></link>'
 
-<<<<<<< HEAD
-		html_obj('head').append(src_str) 
-
-	def add_scripts(self, sources):
-		html_obj = self.html_obj 
-=======
 		self.html_obj('head').append(src_str) 
 
 	def add_scripts(self, sources): 
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 		src_str = "";
 		for src in sources:
 			src_str += '<script type="text/javascript" src="' + src + '"></script>'
 
-<<<<<<< HEAD
-		html_obj('head').prepend(src_str)
-=======
 		self.html_obj('head').prepend(src_str)
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
  
  	# TAG FUNCTIONS
  	def insert_element_tags(self): 
@@ -90,36 +61,6 @@ class Converter:
 
  			self.dzid += 1
 
-<<<<<<< HEAD
- 			if element_is_type(this, ['script', 'title', 'head', 'header', 'body', 'footer', 'link', 'style', 'meta']):
- 				return 
-	
-			# add text tag
-			element = PyQuery(this)
-			if len(element.text().strip(' \t\n\r')) > 0: 
-				if  this.tag == 'div' and sum(1 for x in element.items('div')) == 1:
-					this.set('dztype', 'text') 
-				else:
-					if (this.text is not None and len(this.text.strip(' \t\n\r')) > 0):
-						this.set('dztype', 'text')
-					if (this.tail is not None and len(this.tail.strip(' \t\n\r')) > 0):
-						new_element = self.new_text_element(this.tail, self.dzid)
-						this.tail = ''
-						this.addnext(new_element)
-						self.dzid += 1
-
-
-			this.set('dzid', str(self.dzid))
-
- 		html_obj = self.html_obj
- 		html_obj('*').each(assign_tag)
-
-	# TEXT FUNCTIONS
-	def update_text(self, target, value):
-		html_obj = self.html_obj
-   
-		elements = html_obj('*').filter('[dzid="' + target + '"]')  
-=======
  			if element_is_type(this, ['script', 'title', 'head', 'header', 'body', 'footer', 'link', 'style', 'meta', 'p', 'b', 'i', 'u', 'strong', 'em']):
  				return 
 
@@ -130,19 +71,11 @@ class Converter:
 	# TEXT FUNCTIONS
 	def update_text(self, target, value):
 		elements = self.html_obj('*').filter('[dzid="' + target + '"]')  
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 
 		for e in elements:  
 	 		pq = PyQuery(e)
 	 		pq.empty()
 	 		pq.append(value)
-<<<<<<< HEAD
-	 		break
-	 		 
-	def new_text_element(self, text, ident):
-		element = etree.Element('dztag')
-		element.set('dztype', 'text') 
-=======
 	 		return True
 
 	 	return False
@@ -150,7 +83,6 @@ class Converter:
 	def new_text_element(self, text, ident):
 		element = etree.Element('dztag')
 		#element.set('dztype', 'text') 
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 		element.set('dzid', str(ident))
 		element.text = text
 		return element
@@ -162,16 +94,8 @@ class Converter:
 
 
 	def replace_link(self, attr):
-<<<<<<< HEAD
-		html_obj = self.html_obj
-
-		location = Constants.S3_TEMPLATE_URL + self.template_name + '/'
-
-		elements = html_obj('*').filter('[' + attr + ']')
-=======
 		elements = self.html_obj('*').filter('[' + attr + ']')
 
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 		for e in elements:
 			value = e.get(attr);
 
@@ -182,11 +106,6 @@ class Converter:
 			if file_extension == '' or file_extension.find('.html') > 0:
 				continue
 
-<<<<<<< HEAD
-			value = location + value
-			e.set(attr, value)
-	  
-=======
 			value = self.location + value
 			e.set(attr, value)
 	  
@@ -206,7 +125,6 @@ class Converter:
 
 		return None
 		
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 
 # HELPER FUNCTIONS
 def unique_text_id(num):
@@ -217,11 +135,7 @@ def is_absolute(url):
 
 def element_is_type(element,type_arr):
 	for e_type in type_arr:
-<<<<<<< HEAD
-		if PyQuery(element).is_(e_type):
-=======
 		if PyQuery(element).clone().empty().is_(e_type):
->>>>>>> b4c5066472c744c20361f3c38e2a1b0a1d679580
 			return True
 	return False
 
