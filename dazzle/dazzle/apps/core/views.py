055965bd48176b6b00d2164334d294efc55f67ac
KirushanthS <kirushanth.2@gmail.com>
2013-09-12 19:39:51 -0400

    added views for register and login

--- dazzle/dazzle/apps/core/views.py
+++ dazzle/dazzle/apps/core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def login(request):
+	return render(request, 'core/login.html')
+
+def register(request):
+	return render(request, 'core/register.html')
